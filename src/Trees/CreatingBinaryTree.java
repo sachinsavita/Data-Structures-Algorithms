bbfe5f9468f9607e1522213435fdfbb240ed60f1
sachinsavita <sachinsavita95@gmail.com>
2022-07-13 19:58:55 +0530

    TreeUpdated

--- src/Trees/CreatingBinaryTree.java
+++ src/Trees/CreatingBinaryTree.java
@@ -119,13 +119,64 @@ public class CreatingBinaryTree {
 					}
 				}
 	
-	
+	//countNodes
+				public static int countNodes(Node root) // time complexity = O(n)
+				{
+					if(root==null)
+					{
+						return 0;
+					}
+					
+					int leftNodes = countNodes(root.left);
+					int rightNodes = countNodes(root.right);
+					
+					return leftNodes + rightNodes + 1;
+					
+				}
+				
+				
+	//Sum of Nodes
+				public static int sumOfNodes(Node root) // time complexity = O(n)
+				{	
+					if(root==null)
+					{
+						return 0;
+					}
+					
+					int leftNodesSum = sumOfNodes(root.left);
+					int rightNodesSum = sumOfNodes(root.right);
+					
+					return leftNodesSum + rightNodesSum + root.data;
+					
+				} 
+				
+	//Height of tree
+				public static int heightOfTree(Node root) // time complexity = O(n)
+				{	
+					if(root==null)
+					{
+						return 0;
+					}
+					
+					int leftTreeHeight = heightOfTree(root.left);
+					int rightTreeHeight = heightOfTree(root.right);
+					
+					int maxHeight = Math.max(leftTreeHeight, rightTreeHeight)+1;
+					
+					return maxHeight;
+					
+//					if(leftTreeHeight>rightTreeHeight)
+//						return leftTreeHeight+1;
+//					else
+//						return rightTreeHeight+1;
+				} 
+				
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 		BinaryTree tree = new BinaryTree();
 		Node root = tree.buildTree(nodes);
-		levelOrder(root);
+		System.out.println(heightOfTree(root));
 
 	}
 
