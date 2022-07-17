d8a67219550fc9b6d3674ad33fa69be450c9f29d
sachinsavita <sachinsavita95@gmail.com>
2022-07-17 23:51:48 +0530

    Update

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+	
+	public static void main(String args[])
+	{
+		int n = 192;
+		long multi2 = n*2;
+        long multi3 = n*3;
+        
+        String str = String.valueOf(n);
+        str = str+multi2+multi3;
+        
+        System.out.println(str);
+	}
+
+}
