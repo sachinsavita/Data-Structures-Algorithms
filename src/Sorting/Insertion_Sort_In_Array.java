2d9f47edd68761ed80220b4bc43da6a759cd353c
sachinsavita <sachinsavita95@gmail.com>
2022-07-18 12:17:01 +0530

    upadted

--- src/Sorting/Insertion_Sort_In_Array.java
+++ src/Sorting/Insertion_Sort_In_Array.java
@@ -5,23 +5,23 @@ public class Insertion_Sort_In_Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr[] = {8,5,3,7,9,1};
+		int arr[] = {8,5,3,7,9,1,56,23,45,45,45,45,12,78,95,34,27,98,67,83,67};
 		int key = 0;
 		for(int i=1;i<arr.length;i++)
 		{
 			int j = i-1;
 			key = arr[i];
-			while(j>=0 && arr[j]>key)
+			while(j>=0 && arr[j]>key)      //j>=0      or   j>-1
 			{
-				arr[j+1] = arr[j];
+				arr[j+1] = arr[j]; //Shifting elements till not find shorter element than key 
 				j--;
 			}
-			arr[j+1] = key;
+			arr[j+1] = key;   //After shifting j+1 space id vacant 
 		}
 		
 		for(int x : arr)
 		{
-			System.out.println(x+" ");
+			System.out.print(x+" ");
 		}
 
 	}
---
+++ src/Sorting/Insertion_Sort_In_LinkedList.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+public class Insertion_Sort_In_LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+
+	}
+
+}
